13f302794aad2c1ec1f9abe7dc4561d88d409e69
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-07-20 20:58:53 +0530

    Add options A, B, C, D to the buttons in Quiz Activity

--- app/src/main/java/com/rohit/suthar/quizapp/activities/QuizDashboardActivity.java
+++ app/src/main/java/com/rohit/suthar/quizapp/activities/QuizDashboardActivity.java
@@ -1,15 +1,21 @@
 package com.rohit.suthar.quizapp.activities;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.rohit.suthar.quizapp.R;
 
-public class MainActivity extends AppCompatActivity {
+public class QuizDashboardActivity extends AppCompatActivity {
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quiz_dashboard);
+    }
+
+    private void initialize() {
+        mContext = QuizDashboardActivity.this;
     }
 }
--- app/src/main/java/com/rohit/suthar/quizapp/models/QuestionHolder.java
+++ app/src/main/java/com/rohit/suthar/quizapp/models/QuestionHolder.java
@@ -1,5 +1,13 @@
 package com.rohit.suthar.quizapp.models;
 
 public class QuestionHolder {
+    private String question;
+    private String optionA;
+    private String optionB;
+    private String optionC;
+    private String optionD;
+    private String optionCorrect;
+
+
 
 }
