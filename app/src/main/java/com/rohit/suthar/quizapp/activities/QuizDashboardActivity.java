841b9680c2e9dcadac012b57dc74eee05a987b55
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-07-24 18:10:41 +0530

    background image color is changed to light color and added model for questions

--- app/src/main/java/com/rohit/suthar/quizapp/activities/QuizDashboardActivity.java
+++ app/src/main/java/com/rohit/suthar/quizapp/activities/QuizDashboardActivity.java
@@ -3,19 +3,27 @@ package com.rohit.suthar.quizapp.activities;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.rohit.suthar.quizapp.R;
 
 public class QuizDashboardActivity extends AppCompatActivity {
     private Context mContext;
+    private TextView mTvQuestionNo, mTvQuestion;
+    private Button mBtnOptionA, mBtnOptionB, mBtnOptionC, mBtnOptionD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz_dashboard);
+        initialize();
     }
 
     private void initialize() {
         mContext = QuizDashboardActivity.this;
+        mTvQuestion = findViewById(R.id.tv_question);
+        mTvQuestionNo = findViewById(R.id.tv_question_no);
+
     }
 }
--- app/src/main/java/com/rohit/suthar/quizapp/models/QuestionHolder.java
+++ app/src/main/java/com/rohit/suthar/quizapp/models/QuestionHolder.java
@@ -9,5 +9,37 @@ public class QuestionHolder {
     private String optionCorrect;
 
 
+    public QuestionHolder(String question, String optionA, String optionB, String optionC, String optionD, String optionCorrect) {
+        this.question = question;
+        this.optionA = optionA;
+        this.optionB = optionB;
+        this.optionC = optionC;
+        this.optionD = optionD;
+        this.optionCorrect = optionCorrect;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getOptionA() {
+        return optionA;
+    }
+
+    public String getOptionB() {
+        return optionB;
+    }
+
+    public String getOptionC() {
+        return optionC;
+    }
+
+    public String getOptionD() {
+        return optionD;
+    }
+
+    public String getOptionCorrect() {
+        return optionCorrect;
+    }
 
 }
