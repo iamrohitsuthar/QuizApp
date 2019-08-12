883b475d33c3ec323ed7e8b84f50513ea16aee3e
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-12 22:59:33 +0530

    add SplashActivity and its UI file

---
+++ app/src/main/java/com/rohit/suthar/quizapp/activities/SplashActivity.java
@@ -0,0 +1,27 @@
+package com.rohit.suthar.quizapp.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.rohit.suthar.quizapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,QuizDashboardActivity.class);
+                finish();
+                startActivity(intent);
+            }
+        },1200);
+    }
+}
