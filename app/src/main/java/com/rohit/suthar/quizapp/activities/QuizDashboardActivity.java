0d93b9b7cc6772d8185855cd82f50a701067b541
Rohit Suthar <iamrohitsuthar@gmail.com>
2019-08-12 22:52:59 +0530

    add hash map to hold the mapping between options and respective buttons. clear the buttons background to default after updating next question. Update question function is called in thread after 800ms so correct and wrong selected option of previous button is visible to the user for 800ms. Change Question checking logic add separate onclick attribute function for each option button.

--- app/src/main/java/com/rohit/suthar/quizapp/activities/QuizDashboardActivity.java
+++ app/src/main/java/com/rohit/suthar/quizapp/activities/QuizDashboardActivity.java
@@ -1,17 +1,19 @@
 package com.rohit.suthar.quizapp.activities;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.rohit.suthar.quizapp.R;
 import com.rohit.suthar.quizapp.models.QuestionHolder;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class QuizDashboardActivity extends AppCompatActivity {
     private Context mContext;
@@ -21,6 +23,7 @@ public class QuizDashboardActivity extends AppCompatActivity {
     private int questionTracker;
     private Button which;
     private boolean isCorrect = false;
+    private HashMap<String,Button> optionsMapping;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,80 +38,48 @@ public class QuizDashboardActivity extends AppCompatActivity {
         mContext = QuizDashboardActivity.this;
         mTvQuestion = findViewById(R.id.tv_question);
         mTvQuestionNo = findViewById(R.id.tv_question_no);
-        mBtnOptionA = findViewById(R.id.btn_a);
-        mBtnOptionB = findViewById(R.id.btn_b);
-        mBtnOptionC = findViewById(R.id.btn_c);
-        mBtnOptionD = findViewById(R.id.btn_d);
+        mBtnOptionA = findViewById(R.id.a);
+        mBtnOptionB = findViewById(R.id.b);
+        mBtnOptionC = findViewById(R.id.c);
+        mBtnOptionD = findViewById(R.id.d);
         questionTracker = 0;
         questionHolderArrayList = new ArrayList<>();
+        optionsMapping = new HashMap<>();
+        optionsMapping.put("a",mBtnOptionA);
+        optionsMapping.put("b",mBtnOptionB);
+        optionsMapping.put("c",mBtnOptionC);
+        optionsMapping.put("d",mBtnOptionD);
     }
 
     private void loadQuestions() {
-            mTvQuestionNo.setText(String.valueOf(questionTracker+1));
-            mTvQuestion.setText(questionHolderArrayList.get(questionTracker).getQuestion());
-            mBtnOptionA.setText(questionHolderArrayList.get(questionTracker).getOptionA());
-            mBtnOptionB.setText(questionHolderArrayList.get(questionTracker).getOptionB());
-            mBtnOptionC.setText(questionHolderArrayList.get(questionTracker).getOptionC());
-            mBtnOptionD.setText(questionHolderArrayList.get(questionTracker).getOptionD());
-            checkQuestion();
+        mTvQuestionNo.setText(String.valueOf(questionTracker+1));
+        mTvQuestion.setText(questionHolderArrayList.get(questionTracker).getQuestion());
+        mBtnOptionA.setText(questionHolderArrayList.get(questionTracker).getOptionA());
+        mBtnOptionB.setText(questionHolderArrayList.get(questionTracker).getOptionB());
+        mBtnOptionC.setText(questionHolderArrayList.get(questionTracker).getOptionC());
+        mBtnOptionD.setText(questionHolderArrayList.get(questionTracker).getOptionD());
     }
 
-    private void checkQuestion() {
-        mBtnOptionA.setOnClickListener(new View.OnClickListener() {
+    private void checkQuestion(final String options) {
+        boolean isCorrect = true;
+        if(options.trim().equals(questionHolderArrayList.get(questionTracker).getOptionCorrect())) {
+            optionsMapping.get(options).setBackground(getResources().getDrawable(R.drawable.custom_button_correct));
+        }
+        else {
+            optionsMapping.get(options).setBackground(getResources().getDrawable(R.drawable.custom_button_wrong));
+            optionsMapping.get(questionHolderArrayList.get(questionTracker).getOptionCorrect().trim()).setBackground(getResources().getDrawable(R.drawable.custom_button_correct));
+            isCorrect = false;
+        }
+        final boolean finalIsCorrect = isCorrect;
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                if(questionHolderArrayList.get(questionTracker).getOptionCorrect().equals(mBtnOptionA.getText().toString())) {
-                    //correct
-                    mBtnOptionA.setBackground(getResources().getDrawable(R.drawable.custom_button_correct));
-                }
-                else {
-                    mBtnOptionA.setBackground(getResources().getDrawable(R.drawable.custom_button_wrong));
-                }
+            public void run() {
+                if(!finalIsCorrect)
+                    optionsMapping.get(options).setBackground(getResources().getDrawable(R.drawable.custom_button));
+                optionsMapping.get(questionHolderArrayList.get(questionTracker).getOptionCorrect().trim()).setBackground(getResources().getDrawable(R.drawable.custom_button));
                 updateQuestion();
             }
-        });
-
-        mBtnOptionB.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(questionHolderArrayList.get(questionTracker).getOptionCorrect().equals(mBtnOptionB.getText().toString())) {
-                    //correct
-                    mBtnOptionB.setBackground(getResources().getDrawable(R.drawable.custom_button_correct));
-                }
-                else {
-                    mBtnOptionB.setBackground(getResources().getDrawable(R.drawable.custom_button_wrong));
-                }
-                updateQuestion();
-            }
-        });
-
-        mBtnOptionC.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(questionHolderArrayList.get(questionTracker).getOptionCorrect().equals(mBtnOptionC.getText().toString())) {
-                    //correct
-                    mBtnOptionC.setBackground(getResources().getDrawable(R.drawable.custom_button_correct));
-                }
-                else {
-                    mBtnOptionC.setBackground(getResources().getDrawable(R.drawable.custom_button_wrong));
-                }
-                updateQuestion();
-            }
-        });
-
-        mBtnOptionD.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(questionHolderArrayList.get(questionTracker).getOptionCorrect().equals(mBtnOptionD.getText().toString())) {
-                    //correct
-                    mBtnOptionD.setBackground(getResources().getDrawable(R.drawable.custom_button_correct));
-                }
-                else {
-                    mBtnOptionD.setBackground(getResources().getDrawable(R.drawable.custom_button_wrong));
-                }
-                updateQuestion();
-            }
-        });
+        },800);
     }
 
     private void updateQuestion() {
@@ -122,5 +93,21 @@ public class QuizDashboardActivity extends AppCompatActivity {
     private void addQuestions() {
         questionHolderArrayList.add(new QuestionHolder(getResources().getString(R.string.q1),getResources().getString(R.string.q1_a),getResources().getString(R.string.q1_b),getResources().getString(R.string.q1_c),getResources().getString(R.string.q1_d),getResources().getString(R.string.q1_ans)));
         questionHolderArrayList.add(new QuestionHolder(getResources().getString(R.string.q2),getResources().getString(R.string.q2_a),getResources().getString(R.string.q2_b),getResources().getString(R.string.q2_c),getResources().getString(R.string.q2_d),getResources().getString(R.string.q2_ans)));
+    }
+
+    public void optionAClicked(View view) {
+        checkQuestion("a");
+    }
+
+    public void optionBClicked(View view) {
+        checkQuestion("b");
+    }
+
+    public void optionCClicked(View view) {
+        checkQuestion("c");
+    }
+
+    public void optionDClicked(View view) {
+        checkQuestion("d");
     }
 }
